b03963a59f5f18e65d007911c30d9e5f380404a4
kuroweb <kuromitsu0104@gmail.com>
2023-10-26 09:04:09 +0900

    feat: graphqlコンテナを追加

---
+++ volumes/graphql/server.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/jinzhu/gorm"
+	"github.com/kuroweb/price-monitoring/volumes/graphql/graph"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+const dataSource = "root:@tcp(db:3306)/graphql?charset=utf8&parseTime=True&loc=Local"
+const defaultPort = "8080"
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = defaultPort
+	}
+
+	db, err := gorm.Open("mysql", dataSource)
+	if err != nil {
+		panic(err)
+	}
+	if db == nil {
+		panic(err)
+	}
+	defer func() {
+		if db != nil {
+			if err := db.Close(); err != nil {
+				panic(err)
+			}
+		}
+	}()
+	db.LogMode(true)
+
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
+
+	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
