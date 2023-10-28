58582377a1b4845f48ed6fb9112e13a985f366b8
kuroweb <kuromitsu0104@gmail.com>
2023-10-28 11:19:54 +0900

    feat: frontendとbackendを疎通させる

--- volumes/frontend/app/page.tsx
+++ volumes/frontend/app/page.tsx
@@ -1,9 +1,23 @@
+"use client"
+
 import Layout from '@/components/layouts/Layout'
+import { useState } from 'react'
 
 const Page = () => {
+  const [data, setData] = useState<any>()
+
+  const fetchData = async () => {
+    const response = await fetch('http://localhost:3000')
+    console.log(response)
+    setData(response.status)
+  }
+
+  fetchData()
+
   return (
     <Layout>
       <p>Dashboard</p>
+      <p>{data}</p>
     </Layout>
   )
 }
