3c29c34ec53323f55483ed77c3312d2f92edb34a
kuroweb <kuromitsu0104@gmail.com>
2023-10-31 08:09:43 +0900

    feat: cors設定を追加する

--- volumes/graphql/server.go
+++ volumes/graphql/server.go
@@ -8,6 +8,7 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/kuroweb/price-monitoring/volumes/graphql/graph"
+	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
@@ -19,9 +20,13 @@ func main() {
 	}
 
 	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	c := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000"},
+		AllowCredentials: true,
+	})
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	http.Handle("/query", c.Handler(srv))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
