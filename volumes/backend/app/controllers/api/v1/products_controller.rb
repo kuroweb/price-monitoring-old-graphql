f8dea9e9c45413091ca043454dc9e8993cef3d80
kuroweb <kuromitsu0104@gmail.com>
2023-11-04 17:05:52 +0900

    feat: #2 products#indexを追加する

---
+++ volumes/backend/app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,26 @@
+module Api
+  module V1
+    class ProductsController < ApplicationController
+      def index
+        products = ProductFinder.new(params: product_params).execute
+        render json: { products: products.as_json }, status: 200
+      end
+
+      def create; end
+
+      def update; end
+
+      def destroy; end
+
+      private
+
+      def product_params_attributes
+        %i[id user_id name price]
+      end
+
+      def product_params
+        @product_params ||= params.require(:product).permit(product_params_attributes)
+      end
+    end
+  end
+end
