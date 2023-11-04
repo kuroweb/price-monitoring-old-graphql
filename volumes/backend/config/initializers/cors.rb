8f69d44f0876c0b29c76311208ace2b945846dc7
kuroweb <kuromitsu0104@gmail.com>
2023-11-04 22:30:53 +0900

    feat: #5 不要なoriginを削除する

--- volumes/backend/config/initializers/cors.rb
+++ volumes/backend/config/initializers/cors.rb
@@ -1,8 +1,5 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # TODO: 環境変数で取り扱う
-    origins "localhost:8080"
-
     resource "*",
              headers: :any,
              methods: %i[get post put patch delete options head]
