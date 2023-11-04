7e22be73d7f4f1af59c88df918053100c3c6fab2
kuroweb <kuromitsu0104@gmail.com>
2023-11-04 16:35:08 +0900

    feat: #2 Productモデルを追加する

--- volumes/backend/db/schema.rb
+++ volumes/backend/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_03_002239) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_04_072910) do
+  create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.text "name"
     t.datetime "created_at", null: false
