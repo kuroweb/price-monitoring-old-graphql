87baab5c5094cdcba0713853678dfa3590ac918b
kuroweb <kuromitsu0104@gmail.com>
2023-11-07 00:43:42 +0900

    feat: #8 パンくずを追加する

--- volumes/frontend/app/products/page.tsx
+++ volumes/frontend/app/products/page.tsx
@@ -8,12 +8,24 @@ import { GetProductsDocument, GetProductsQuery } from '@/graphql/dist/client'
 const Page = () => {
   const { data, loading, error } = useQuery<GetProductsQuery>(GetProductsDocument, { variables: { user_id: 1 } })
 
-  if (loading) return <div>LOADING...</div>
-  if (error) return <div>{error.message}</div>
-
   return (
     <Layout>
-      <p>Products</p>
+      {/* TODO: 別コンポーネントに切り出す */}
+      <ol className="flex items-center whitespace-nowrap min-w-0" aria-label="Breadcrumb">
+        <li className="text-sm text-gray-500">
+          <div className="flex items-center">
+            <span className="font-semibold">Products</span>
+          </div>
+        </li>
+      </ol>
+      {/* TODO: 別コンポーネントに切り出す */}
+      {loading && (
+        <div>Loading...</div>
+      )}
+      {/* TODO: 別コンポーネントに切り出す */}
+      {error && (
+        <div>{error.message}</div>
+      )}
       {data?.getProducts.map((product) => (
         <p key={product.id}>
           {product.id}, {product.user_id}, {product.name}, {product.price}
