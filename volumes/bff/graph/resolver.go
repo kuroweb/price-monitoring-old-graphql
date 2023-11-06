f31e559a06324e10b4ee219498e34699bfb88efe
kuroweb <kuromitsu0104@gmail.com>
2023-11-07 00:05:18 +0900

    feat: #8 resolverを空にする

--- volumes/bff/graph/resolver.go
+++ volumes/bff/graph/resolver.go
@@ -1,13 +1,7 @@
 package graph
 
-import "github.com/kuroweb/price-monitoring/volumes/bff/graph/model"
-
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct {
-	todos    []*model.Todo
-	users    []*model.User
-	products []*model.Product
-}
+type Resolver struct{}
