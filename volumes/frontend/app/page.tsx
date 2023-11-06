f58ec02f264320468a55a4cba6604eefc4ca91ce
kuroweb <kuromitsu0104@gmail.com>
2023-11-07 08:32:55 +0900

    feat: #8 不要コードを削除する

--- volumes/frontend/app/page.tsx
+++ volumes/frontend/app/page.tsx
@@ -1,24 +1,11 @@
 'use client'
 
-import { useQuery } from '@apollo/client'
 import Layout from '@/components/layouts/Layout'
 
-import { GetTodoDocument, GetTodoQuery } from '@/graphql/dist/client'
-
 const Page = () => {
-  const { data, loading, error } = useQuery<GetTodoQuery>(GetTodoDocument)
-
-  if (loading) return <div>LOADING...</div>
-  if (error) return <div>{error.message}</div>
-
   return (
     <Layout>
       <p>Dashboard</p>
-      {data?.todos.map((todo) => (
-        <p key={todo.id}>
-          {todo.id}: {todo.text}
-        </p>
-      ))}
     </Layout>
   )
 }
