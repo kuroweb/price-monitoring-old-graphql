730faa8ebfb92a89e4b3dcd5057c375c07521020
kuroweb <kuromitsu0104@gmail.com>
2023-11-30 09:25:15 +0900

    feat: #10 socks経由でスクレイピングできるようにする

--- volumes/backend/lib/crawl/client.rb
+++ volumes/backend/lib/crawl/client.rb
@@ -2,7 +2,11 @@ module Crawl
   class Client
     def self.execute(&block)
       Playwright.connect_to_playwright_server("ws://playwright:8888/ws") do |playwright|
-        playwright.chromium.launch(&block)
+        playwright.chromium.launch(
+          headless: true,
+          proxy: { server: "socks5://tor:9050" },
+          &block
+        )
       end
     end
   end
