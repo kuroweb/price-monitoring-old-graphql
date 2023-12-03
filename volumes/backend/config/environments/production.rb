99551bdcf83a8e73fbd1ff96b383f42987c7745a
kuroweb <kuromitsu0104@gmail.com>
2023-12-03 17:54:07 +0900

    feat: #20 Production環境の設定調整

--- volumes/backend/config/environments/production.rb
+++ volumes/backend/config/environments/production.rb
@@ -49,7 +49,8 @@ Rails.application.configure do
   # config.assume_ssl = true
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  # TODO: SSL化は後で対応する
+  config.force_ssl = false
 
   # Log to STDOUT by default
   config.logger = ActiveSupport::Logger.new(STDOUT)
