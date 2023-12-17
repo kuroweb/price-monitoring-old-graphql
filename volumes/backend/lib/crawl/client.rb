fef51165a60d38d509b974ce08e547b482ce26e9
kuroweb <kuromitsu0104@gmail.com>
2023-12-18 08:35:02 +0900

    feat: playwrightコンテナのパスをENV化

--- volumes/backend/lib/crawl/client.rb
+++ volumes/backend/lib/crawl/client.rb
@@ -1,7 +1,8 @@
 module Crawl
   class Client
     def self.execute(&block)
-      Playwright.connect_to_playwright_server("ws://playwright:8888/ws") do |playwright|
+      Playwright.connect_to_playwright_server(ENV.fetch("PLAYWRIGHT_URL")) do |playwright|
+        # TODO: ローカル環境での指定方法を改善する
         url = Rails.env.production? ? ENV.fetch("TOR_URL") : "socks5://tor#{rand(1..10)}:9050"
 
         playwright.chromium.launch(
