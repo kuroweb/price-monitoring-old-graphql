414ad63a2d8c0d79aecd912d96f496fbf8e11cfa
kuroweb <kuromitsu0104@gmail.com>
2023-12-18 21:49:39 +0900

    feat: sidekiqのスレッド数を環境毎に自動切り替えする

--- volumes/backend/config/initializers/sidekiq.rb
+++ volumes/backend/config/initializers/sidekiq.rb
@@ -4,4 +4,7 @@ end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV.fetch("REDIS_URL") }
+
+  config_hash = YAML.load_file(File.join(Rails.root, "config", "sidekiq.yml"))
+  config[:concurrency] = config_hash[Rails.env][:concurrency]
 end
