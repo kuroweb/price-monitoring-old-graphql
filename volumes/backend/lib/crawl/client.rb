ac5fd7733f716b1be1ca19cf8b58fd27d4e84304
kuroweb <kuromitsu0104@gmail.com>
2023-12-19 08:42:25 +0900

    feat: torコンテナを調整

--- volumes/backend/lib/crawl/client.rb
+++ volumes/backend/lib/crawl/client.rb
@@ -3,7 +3,7 @@ module Crawl
     def self.execute(&block)
       Playwright.connect_to_playwright_server(ENV.fetch("PLAYWRIGHT_URL")) do |playwright|
         # TODO: ローカル環境での指定方法を改善する
-        url = Rails.env.production? ? ENV.fetch("TOR_URL") : "socks5://tor#{rand(1..10)}:9050"
+        url = Rails.env.production? ? ENV.fetch("TOR_URL") : "socks5://backend-tor-#{rand(1..5)}:9050"
 
         playwright.chromium.launch(
           headless: true,
