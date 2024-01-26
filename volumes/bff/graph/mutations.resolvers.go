877ce35db2504b1d1a0fc8d42b88d29b7fe076bc
kuroweb <kuromitsu0104@gmail.com>
2024-01-26 09:10:24 +0900

    feat: DELETEクエリを実装

--- volumes/bff/graph/mutations.resolvers.go
+++ volumes/bff/graph/mutations.resolvers.go
@@ -62,30 +62,55 @@ func (r *mutationResolver) CreateProduct(ctx context.Context, input model.Create
 
 // DeleteProduct is the resolver for the deleteProduct field.
 func (r *mutationResolver) DeleteProduct(ctx context.Context, id string) (model.DeleteProductResult, error) {
+	// TODO: 後でリファクタする
 	cfg := config.NewConfig()
 	url := fmt.Sprintf("%s/api/v1/products/%s", cfg.BackendUrl, id)
 	client := &http.Client{}
 
 	req, err := http.NewRequest("DELETE", url, nil)
 	if err != nil {
-		panic(fmt.Errorf("not implemented: DeleteProduct - deleteProduct"))
+		result := model.DeleteProductResultError{
+			Ok: false,
+			Error: model.DeleteProductResultValidationFailed{
+				Code:    "503",
+				Message: "Service is currently unavailable.",
+				Details: []*model.ErrorDetail{},
+			},
+		}
+
+		return result, nil
 	}
 
-	// クライアントを使用してリクエストを送信
+	serverErrorResult := model.DeleteProductResultError{
+		Ok: false,
+		Error: model.DeleteProductResultValidationFailed{
+			Code:    "503",
+			Message: "Service is currently unavailable.",
+			Details: []*model.ErrorDetail{},
+		},
+	}
+
 	resp, err := client.Do(req)
 	if err != nil {
-		panic(fmt.Errorf("not implemented: DeleteProduct - deleteProduct"))
+		return serverErrorResult, nil
 	}
 	defer resp.Body.Close()
 
-	// TODO: 対応中
-	var result model.DeleteProductResult
-	decoder := json.NewDecoder(resp.Body)
-	if err := decoder.Decode(&result); err != nil {
-		return nil, err
+	switch resp.StatusCode {
+	case http.StatusOK:
+		return model.DeleteProductResultSuccess{Ok: true}, nil
+	case http.StatusNotFound:
+		return model.DeleteProductResultError{
+			Ok: false,
+			Error: model.DeleteProductResultValidationFailed{
+				Code:    "404",
+				Message: "Requested resource was not found.",
+				Details: []*model.ErrorDetail{},
+			},
+		}, nil
+	default:
+		return serverErrorResult, nil
 	}
-
-	return result, nil
 }
 
 // Mutation returns internal.MutationResolver implementation.
