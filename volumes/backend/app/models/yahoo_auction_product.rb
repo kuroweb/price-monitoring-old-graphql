32a0e618496fd0df3075454a4b1d270dccf4f554
kuroweb <kuromitsu0104@gmail.com>
2024-02-02 09:14:36 +0900

    docs: コメント追加

--- volumes/backend/app/models/yahoo_auction_product.rb
+++ volumes/backend/app/models/yahoo_auction_product.rb
@@ -1,5 +1,11 @@
 class YahooAuctionProduct < ApplicationRecord
+  ## associations ##
   belongs_to :product
 
+  ## validations ##
+
+  ## scopes ##
   scope :published, -> { where(published: true) }
+
+  ## methods ##
 end
