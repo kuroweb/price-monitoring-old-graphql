b4effaf4a3e2a0b38c0997f99c616417ef906c5e
kuroweb <kuromitsu0104@gmail.com>
2024-02-04 22:55:50 +0900

    feat: 更新サービスを追加

---
+++ volumes/backend/app/services/products/update_service.rb
@@ -0,0 +1,46 @@
+module Products
+  class UpdateService
+    PRODUCT_ATTRIBUTES = %i[name].freeze
+    YAHOO_AUCTION_CRAWL_SETTING_ATTRIBUTES = %i[keyword category_id min_price max_price enabled].freeze
+
+    def self.call(...)
+      new(...).call
+    end
+
+    def initialize(product:, params: {})
+      @product = product
+      @params = params
+    end
+
+    def call
+      ApplicationRecord.transaction do
+        update_product
+        update_yahoo_auction_crawl_setting
+
+        ServiceResponse.success(payload: { product: })
+      end
+    rescue StandardError => e
+      ServiceResponse.error(message: e.message)
+    end
+
+    private
+
+    attr_reader :product, :params
+
+    def update_product
+      product.update(product_params)
+    end
+
+    def update_yahoo_auction_crawl_setting
+      product.yahoo_auction_crawl_setting.update(yahoo_auction_crawl_setting_params)
+    end
+
+    def product_params
+      params.slice(*PRODUCT_ATTRIBUTES) || {}
+    end
+
+    def yahoo_auction_crawl_setting_params
+      params[:yahoo_auction_crawl_setting]&.slice(*YAHOO_AUCTION_CRAWL_SETTING_ATTRIBUTES) || {}
+    end
+  end
+end
