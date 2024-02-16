a7a7b14a70da8116e9e815c9cd745602fbb75da2
kuroweb <kuromitsu0104@gmail.com>
2024-02-17 06:36:35 +0900

    feat: #32 bought_dateカラムを追加

---
+++ volumes/backend/db/migrate/20240216213455_add_bought_date_column_to_yahoo_auction_product.rb
@@ -0,0 +1,5 @@
+class AddBoughtDateColumnToYahooAuctionProduct < ActiveRecord::Migration[7.1]
+  def change
+    add_column :yahoo_auction_products, :bought_date, :datetime, null: true, default: nil
+  end
+end
--- volumes/backend/db/schema.rb
+++ volumes/backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_16_142238) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_16_213455) do
   create_table "products", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -53,6 +53,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_16_142238) do
     t.boolean "published", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "bought_date"
     t.index ["product_id"], name: "index_yahoo_auction_products_on_product_id"
     t.index ["yahoo_auction_id"], name: "index_yahoo_auction_products_on_yahoo_auction_id", unique: true
   end
