54ca19db18d2d72c24b89f03588c22fd10a2d545
kuroweb <kuromitsu0104@gmail.com>
2024-02-19 23:37:44 +0900

    feat: #28 設定更新モーダルのレイアウト調整

--- volumes/frontend/features/products/components/UpdateFormModal.tsx
+++ volumes/frontend/features/products/components/UpdateFormModal.tsx
@@ -64,74 +64,78 @@ const UpdateFormModal = ({ data }: { data: GetProductDetailPageDataQuery }) => {
           >
             ✕
           </div>
-          <h3 className='font-bold text-lg'>更新フォーム</h3>
-          <Tabs className='tabs-boxed'>
-            <Tabs.RadioTab name='hoge' label='Tab' contentClassName='p-10' defaultChecked={true}>
-              Tab content 1
-            </Tabs.RadioTab>
-            <Tabs.RadioTab name='hoge' label='Tab' contentClassName='p-10'>
-              Tab content 2
-            </Tabs.RadioTab>
-            <Tabs.RadioTab name='hoge' label='Tab' contentClassName='p-10'>
-              Tab content 3
-            </Tabs.RadioTab>
-          </Tabs>
-          <form onSubmit={handleSubmit(onSubmit)} className='w-full space-y-2'>
-            <div className='divider py-6'>共通設定</div>
+          <h3 className='font-bold text-lg'>計測設定を更新</h3>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className='divider pt-6'>共通設定</div>
             <label className='form-control'>
               <div className='label'>
                 <span className='label-text'>管理コード</span>
               </div>
               <input {...register('name')} className='input input-bordered' />
             </label>
-            <div className='divider py-6'>ヤフオク設定</div>
-            <label className='form-control'>
-              <div className='label'>
-                <span className='label-text'>計測キーワード</span>
-              </div>
-              <input
-                {...register('yahoo_auction_crawl_setting.keyword')}
-                className='input input-bordered'
-              />
-            </label>
-            <label className='form-control'>
-              <div className='label'>
-                <span className='label-text'>カテゴリID</span>
-              </div>
-              <input
-                {...register('yahoo_auction_crawl_setting.category_id')}
-                className='input input-bordered'
-              />
-            </label>
-            <label className='form-control'>
-              <div className='label'>
-                <span className='label-text'>最低価格</span>
-              </div>
-              <input
-                {...register('yahoo_auction_crawl_setting.min_price')}
-                className='input input-bordered'
-              />
-            </label>
-            <label className='form-control'>
-              <div className='label'>
-                <span className='label-text'>最高価格</span>
-              </div>
-              <input
-                {...register('yahoo_auction_crawl_setting.max_price')}
-                className='input input-bordered'
-              />
-            </label>
-            <label className='label cursor-pointer'>
-              <span className='label-text'>自動計測</span>
-              <input
-                {...register('yahoo_auction_crawl_setting.enabled')}
-                type='checkbox'
-                className='toggle toggle-primary'
-              />
-            </label>
+            <div className='divider py-6'>詳細設定</div>
+            <Tabs className='tabs-boxed'>
+              <Tabs.RadioTab
+                name='update_form_modal'
+                label='ヤフオク'
+                contentClassName='p-4 space-y-4'
+                defaultChecked={true}
+              >
+                <label className='form-control'>
+                  <div className='label'>
+                    <span className='label-text'>計測キーワード</span>
+                  </div>
+                  <input
+                    {...register('yahoo_auction_crawl_setting.keyword')}
+                    className='input input-bordered'
+                  />
+                </label>
+                <label className='form-control'>
+                  <div className='label'>
+                    <span className='label-text'>カテゴリID</span>
+                  </div>
+                  <input
+                    {...register('yahoo_auction_crawl_setting.category_id')}
+                    className='input input-bordered'
+                  />
+                </label>
+                <label className='form-control'>
+                  <div className='label'>
+                    <span className='label-text'>最低価格</span>
+                  </div>
+                  <input
+                    {...register('yahoo_auction_crawl_setting.min_price')}
+                    className='input input-bordered'
+                  />
+                </label>
+                <label className='form-control'>
+                  <div className='label'>
+                    <span className='label-text'>最高価格</span>
+                  </div>
+                  <input
+                    {...register('yahoo_auction_crawl_setting.max_price')}
+                    className='input input-bordered'
+                  />
+                </label>
+                <label className='label cursor-pointer '>
+                  <span className='label-text'>自動計測</span>
+                  <input
+                    {...register('yahoo_auction_crawl_setting.enabled')}
+                    type='checkbox'
+                    className='toggle toggle-primary'
+                  />
+                </label>
+              </Tabs.RadioTab>
+              <Tabs.RadioTab name='update_form_modal' label='メルカリ' contentClassName='p-10'>
+                Tab content 2
+              </Tabs.RadioTab>
+              <Tabs.RadioTab name='update_form_modal' label='ペイペイ' contentClassName='p-10'>
+                Tab content 3
+              </Tabs.RadioTab>
+            </Tabs>
             <button
               type='submit'
-              className='block mt-4 py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-700'
+              className='block w-full mt-6 py-2 px-4 bg-blue-500 text-white rounded-md hover:bg-blue-700'
             >
               更新
             </button>
