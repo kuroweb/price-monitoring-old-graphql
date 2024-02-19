3b0096b4cbcbd71af8fb1d8c9a9d45986384c768
kuroweb <kuromitsu0104@gmail.com>
2024-02-19 18:26:50 +0900

    refactor: 不要コメントを撤去

--- volumes/backend/app/controllers/api/v1/products/yahoo_auction_crawl_settings_controller.rb
+++ volumes/backend/app/controllers/api/v1/products/yahoo_auction_crawl_settings_controller.rb
@@ -3,7 +3,6 @@ module Api
     module Products
       class YahooAuctionCrawlSettingsController < ApplicationController
         def index
-          # TODO: GraphQLで
           yahoo_auction_crawl_setting = YahooAuctionCrawlSetting.find_by(product_id: params[:product_id])
 
           if yahoo_auction_crawl_setting
