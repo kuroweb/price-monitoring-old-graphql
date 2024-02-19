8f0d08e50fc0b4c35e3a8c51633a38a0cad89a93
kuroweb <kuromitsu0104@gmail.com>
2024-02-19 18:28:28 +0900

    refactor: update

--- volumes/backend/app/controllers/api/v1/products/yahoo_auction_crawl_settings/yahoo_auction_crawl_setting_exclude_conditions_controller.rb
+++ volumes/backend/app/controllers/api/v1/products/yahoo_auction_crawl_settings/yahoo_auction_crawl_setting_exclude_conditions_controller.rb
@@ -4,10 +4,11 @@ module Api
       module YahooAuctionCrawlSettings
         class YahooAuctionCrawlSettingExcludeConditionsController < ApplicationController
           def index
-            yahoo_auction_crawl_setting = YahooAuctionCrawlSetting.find_by(product_id: params[:product_id])
-            render json:
-              { yahoo_auction_crawl_setting_exclude_conditions: yahoo_auction_crawl_setting
-                .yahoo_auction_crawl_setting_exclude_conditions }, status: 200
+            yahoo_auction_crawl_setting_exclude_conditions = YahooAuctionCrawlSettingExcludeCondition.where(
+              yahoo_auction_crawl_setting_id: params[:yahoo_auction_crawl_setting_id]
+            )
+
+            render json: { yahoo_auction_crawl_setting_exclude_conditions: }, status: 200
           end
         end
       end
