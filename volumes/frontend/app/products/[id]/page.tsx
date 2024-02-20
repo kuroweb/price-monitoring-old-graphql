09504a77084c73cd63fdba07d3b5a4b624051369
kuroweb <kuromitsu0104@gmail.com>
2024-02-20 19:05:41 +0900

    feat: #28 rename

--- volumes/frontend/app/products/[id]/page.tsx
+++ volumes/frontend/app/products/[id]/page.tsx
@@ -1,6 +1,6 @@
 import Layout from '@/components/layouts/Layout'
 import CrawlSettingModal from '@/features/products/components/CrawlSettingModal'
-import UpdateExcludeConditionModal from '@/features/products/components/UpdateExcludeSettingModal'
+import ExcludeConditionModal from '@/features/products/components/ExcludeConditionModal'
 import CalculateDailyYahooAuctionProductChart from '@/features/products/components/YahooAuctionProductChart'
 import YahooAuctionProductsTable from '@/features/products/components/YahooAuctionProductsTable'
 import {
@@ -23,7 +23,7 @@ const Page = async ({ params }: { params: { id: string } }) => {
             <h2 className='card-title pb-4'>設定</h2>
             <div className='grid grid-cols-2 gap-4'>
               <CrawlSettingModal data={data} />
-              <UpdateExcludeConditionModal data={data} />
+              <ExcludeConditionModal data={data} />
             </div>
           </div>
         </div>
