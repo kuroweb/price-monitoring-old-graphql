2e4dd270f6124c481f63aa6c675ec1e4e00b3254
kuroweb <kuromitsu0104@gmail.com>
2024-02-25 18:27:13 +0900

    feat: #37 バッチ処理を修正

--- volumes/backend/app/services/products/yahoo_auction_products/delete_by_exclude_keyword.rb
+++ volumes/backend/app/services/products/yahoo_auction_products/delete_by_exclude_keyword.rb
@@ -20,13 +20,8 @@ module Products
       attr_reader :product
 
       def deletable
-        product.yahoo_auction_products.merge(or_conditions)
-      end
-
-      def or_conditions
-        price_condition.or(exclude_keywords_conditon)
-                       .or(exclude_yahoo_auction_id_condition)
-                       .or(exclude_seller_id_codition)
+        product.yahoo_auction_products
+               .merge(price_condition.or(exclude_keywords_conditon))
       end
 
       def price_condition
@@ -39,23 +34,10 @@ module Products
                                   .yahoo_auction_crawl_setting_exclude_keywords
                                   .where.not(keyword: nil)
                                   .pluck(:keyword)
-        YahooAuctionProduct.where(name: exclude_keywords)
-      end
 
-      def exclude_yahoo_auction_id_condition
-        ids = product.yahoo_auction_crawl_setting
-                     .yahoo_auction_crawl_setting_exclude_keywords
-                     .where.not(yahoo_auction_id: nil)
-                     .pluck(:yahoo_auction_id)
-        YahooAuctionProduct.where(yahoo_auction_id: ids)
-      end
-
-      def exclude_seller_id_codition
-        ids = product.yahoo_auction_crawl_setting
-                     .yahoo_auction_crawl_setting_exclude_keywords
-                     .where.not(seller_id: nil)
-                     .pluck(:seller_id)
-        YahooAuctionProduct.where(seller_id: ids)
+        exclude_keywords.map do |exclude_keyword|
+          YahooAuctionProduct.where("name LIKE ?", "%#{exclude_keyword}%")
+        end.reduce(&:or)
       end
     end
   end
