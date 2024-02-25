41287751b5fdd3f7683cbce397361cd6214ba89c
kuroweb <kuromitsu0104@gmail.com>
2024-02-25 17:39:52 +0900

    feat: #37 テーブル名を変更する

--- volumes/backend/app/services/crawl/yahoo_auction/inspect_crawl_results.rb
+++ volumes/backend/app/services/crawl/yahoo_auction/inspect_crawl_results.rb
@@ -17,10 +17,7 @@ module Crawl
       attr_reader :yahoo_auction_crawl_setting, :crawl_results
 
       def inspect(crawl_result)
-        inspect_include_keyword(crawl_result) \
-          && inspect_exclude_keyword(crawl_result) \
-          && inspect_exclude_yahoo_auction_id(crawl_result) \
-          && inspect_exclude_seller_id(crawl_result)
+        inspect_include_keyword(crawl_result) && inspect_exclude_keyword(crawl_result)
       end
 
       def inspect_include_keyword(crawl_result)
@@ -31,37 +28,13 @@ module Crawl
         exclude_keywords.all? { |keyword| crawl_result.name.downcase.exclude?(keyword) }
       end
 
-      def inspect_exclude_yahoo_auction_id(crawl_result)
-        exclude_yahoo_auction_ids.exclude?(crawl_result.yahoo_auction_id)
-      end
-
-      def inspect_exclude_seller_id(crawl_result)
-        exclude_seller_ids.exclude?(crawl_result.seller_id)
-      end
-
       def include_keywords
         @include_keywords ||= yahoo_auction_crawl_setting.keyword.downcase.split(" ")
       end
 
       def exclude_keywords
         @exclude_keywords ||=
-          yahoo_auction_crawl_setting
-          .yahoo_auction_crawl_setting_exclude_keywords.where.not(keyword: nil)
-          .pluck(:keyword).map(&:downcase)
-      end
-
-      def exclude_yahoo_auction_ids
-        @exclude_yahoo_auction_ids ||=
-          yahoo_auction_crawl_setting
-          .yahoo_auction_crawl_setting_exclude_keywords.where.not(yahoo_auction_id: nil)
-          .pluck(:yahoo_auction_id)
-      end
-
-      def exclude_seller_ids
-        @exclude_seller_ids ||=
-          yahoo_auction_crawl_setting
-          .yahoo_auction_crawl_setting_exclude_keywords.where.not(seller_id: nil)
-          .pluck(:seller_id)
+          yahoo_auction_crawl_setting.yahoo_auction_crawl_setting_exclude_keywords.where.not(keyword: nil)
       end
     end
   end
