7b4c888f4d27e2c0d0d0af418690d2b7098cf5c9
kuroweb <kuromitsu0104@gmail.com>
2024-02-26 08:47:44 +0900

    feat: #36 crawl_settingのkeywordも対応させる

--- volumes/backend/app/services/products/yahoo_auction_products/delete_by_crawl_setting.rb
+++ volumes/backend/app/services/products/yahoo_auction_products/delete_by_crawl_setting.rb
@@ -1,7 +1,7 @@
 # NOTE: 除外設定とマッチするレコードを撤去する
 module Products
   module YahooAuctionProducts
-    class DeleteByExcludeKeyword
+    class DeleteByCrawlSetting
       def self.call(...)
         new(...).call
       end
@@ -19,13 +19,26 @@ module Products
       attr_reader :product
 
       def deletable
-        product.yahoo_auction_products
-               .merge(price_condition.or(exclude_keywords_conditon))
+        YahooAuctionProduct
+          .where(product_id: product.id)
+          .merge(
+            price_condition
+              .or(include_keywords_condition)
+              .or(exclude_keywords_conditon)
+          )
       end
 
       def price_condition
         price_range = product.yahoo_auction_crawl_setting.min_price..product.yahoo_auction_crawl_setting.max_price
         YahooAuctionProduct.where.not(price: price_range)
+      end
+
+      def include_keywords_condition
+        include_keywords = product.yahoo_auction_crawl_setting.keyword.split(" ")
+
+        include_keywords.map do |include_keyword|
+          YahooAuctionProduct.where.not("name LIKE ?", "%#{include_keyword}%")
+        end.reduce(&:or)
       end
 
       def exclude_keywords_conditon
