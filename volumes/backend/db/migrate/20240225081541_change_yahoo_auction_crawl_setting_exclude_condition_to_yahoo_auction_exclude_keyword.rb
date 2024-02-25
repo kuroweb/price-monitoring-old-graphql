81f234c052cbe1505a29f1005cc272db07b48280
kuroweb <kuromitsu0104@gmail.com>
2024-02-25 18:01:54 +0900

    feat: #37 テーブル名を変更する

--- volumes/backend/db/migrate/20240225081541_change_yahoo_auction_crawl_setting_exclude_condition_to_yahoo_auction_exclude_keyword.rb
+++ volumes/backend/db/migrate/20240225081541_change_yahoo_auction_crawl_setting_exclude_condition_to_yahoo_auction_exclude_keyword.rb
@@ -1,5 +1,5 @@
-class ChangeYahooAuctionCrawlSettingExcludeKeywordToYahooAuctionExcludeKeyword < ActiveRecord::Migration[7.1]
+class ChangeYahooAuctionCrawlSettingExcludeConditionToYahooAuctionExcludeKeyword < ActiveRecord::Migration[7.1]
   def change
-    rename_table :yahoo_auction_crawl_setting_exclude_keywords, :yahoo_auction_crawl_setting_exclude_keywords
+    rename_table :yahoo_auction_crawl_setting_exclude_conditions, :yahoo_auction_crawl_setting_exclude_keywords
   end
 end
