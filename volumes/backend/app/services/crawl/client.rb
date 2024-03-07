60c47b17844b658744543a3a20dd8650dbde70cd
kuroweb <kuromitsu0104@gmail.com>
2024-03-07 09:18:22 +0900

    feat: #51 proxyを参照させる

--- volumes/backend/app/services/crawl/client.rb
+++ volumes/backend/app/services/crawl/client.rb
@@ -2,12 +2,13 @@ module Crawl
   class Client
     def self.execute(&block)
       Playwright.connect_to_playwright_server(ENV.fetch("PLAYWRIGHT_URL")) do |playwright|
-        # TODO: ローカル環境での指定方法を改善する
-        url = Rails.env.production? ? ENV.fetch("TOR_URL") : "socks5://backend-tor-#{rand(1..5)}:9050"
-
         playwright.chromium.launch(
           headless: true,
-          proxy: { server: url },
+          proxy: {
+            server: ENV.fetch("PROXY_HOST"),
+            username: ENV.fetch("PROXY_USERNAME"),
+            password: ENV.fetch("PROXY_PASSWORD")
+          },
           &block
         )
       end
