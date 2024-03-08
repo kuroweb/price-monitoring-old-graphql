cdea3b041cb46f5490b5a64c9c6d2a67e8d36761
kuroweb <kuromitsu0104@gmail.com>
2024-03-09 02:40:05 +0900

    feat: #51 落札日時を取得するクラスを追加

--- volumes/backend/app/models/mercari_product.rb
+++ volumes/backend/app/models/mercari_product.rb
@@ -6,6 +6,7 @@ class MercariProduct < ApplicationRecord
 
   ## scopes ##
   scope :published, -> { where(published: true) }
+  scope :unpublished, -> { where(published: false) }
   scope :bought, -> { where.not(bought_date: nil) }
 
   ## methods ##
