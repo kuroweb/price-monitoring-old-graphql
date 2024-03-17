920adb977556bb70609c67967c629adf6aed7849
kuroweb <kuromitsu0104@gmail.com>
2024-03-17 17:14:29 +0900

    feat: 不要コードを撤去

--- volumes/backend/config/routes.rb
+++ volumes/backend/config/routes.rb
@@ -4,7 +4,6 @@ require "sidekiq/cron/web"
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users
       resources :products
 
       namespace :products do
