d237d98b290c5dfa8630d98e32dfca8152a2ea4c
kuroweb <kuromitsu0104@gmail.com>
2024-03-28 06:51:21 +0900

    feat: レイアウト調整

--- volumes/frontend/features/products/components/create-crawl-setting-modal/CreateCrawlSettingModal.tsx
+++ volumes/frontend/features/products/components/create-crawl-setting-modal/CreateCrawlSettingModal.tsx
@@ -60,7 +60,7 @@ const CreateCrawlSettingModal = () => {
         onChange={(e) => setModal(e.target.checked)}
       />
       <div className='modal' role='dialog'>
-        <div className='modal-box h-3/4 md:h-1/2'>
+        <div className='modal-box h-fit'>
           <div
             onClick={() => setModal(false)}
             className='btn btn-sm btn-circle btn-ghost absolute right-4 top-4'
