7400de50ac0dafd5f90bdb19941c3d157e289ac8
kuroweb <kuromitsu0104@gmail.com>
2024-03-29 02:24:35 +0900

    feat: 検証処理で必須キーワードを参照させる

--- volumes/backend/app/services/crawl/mercari/sync_products/inspect_crawl_results.rb
+++ volumes/backend/app/services/crawl/mercari/sync_products/inspect_crawl_results.rb
@@ -18,15 +18,33 @@ module Crawl
         attr_reader :mercari_crawl_setting, :crawl_results
 
         def inspect(crawl_result)
-          inspect_exclude_keyword(crawl_result)
+          inspect_required_keyword(crawl_result) && inspect_exclude_keyword(crawl_result)
+        end
+
+        def inspect_required_keyword(crawl_result)
+          required_keywords.all? { |keyword| normalize_keyword(crawl_result.name).include?(keyword) }
         end
 
         def inspect_exclude_keyword(crawl_result)
-          exclude_keywords.all? { |keyword| crawl_result.name.downcase.exclude?(keyword) }
+          exclude_keywords.all? { |keyword| normalize_keyword(crawl_result.name).exclude?(keyword) }
+        end
+
+        def required_keywords
+          @required_keywords ||=
+            mercari_crawl_setting.mercari_crawl_setting_required_keywords
+                                 .pluck(:keyword)
+                                 .map { |keyword| normalize_keyword(keyword) }
         end
 
         def exclude_keywords
-          @exclude_keywords ||= mercari_crawl_setting.mercari_crawl_setting_exclude_keywords.pluck(:keyword)
+          @exclude_keywords ||=
+            mercari_crawl_setting.mercari_crawl_setting_exclude_keywords
+                                 .pluck(:keyword)
+                                 .map { |keyword| normalize_keyword(keyword) }
+        end
+
+        def normalize_keyword(keyword)
+          keyword.tr("Ａ-Ｚ０-９", "A-Z0-9").downcase
         end
       end
     end
--- volumes/backend/app/services/products/mercari_products/delete_by_crawl_setting.rb
+++ volumes/backend/app/services/products/mercari_products/delete_by_crawl_setting.rb
@@ -23,6 +23,7 @@ module Products
           .where(product_id: product.id)
           .merge(
             price_condition
+              .or(required_keywords_condition)
               .or(exclude_keywords_conditon)
           )
       end
@@ -32,16 +33,40 @@ module Products
         MercariProduct.where.not(price: price_range)
       end
 
+      def required_keywords_condition
+        return MercariProduct.none if required_keywords.blank?
+
+        required_keywords.map do |required_keyword|
+          MercariProduct.where.not("name LIKE ?", "%#{required_keyword}%")
+        end.reduce(&:or)
+      end
+
       def exclude_keywords_conditon
-        exclude_keywords = product.mercari_crawl_setting
-                                  .mercari_crawl_setting_exclude_keywords
-                                  .where.not(keyword: nil)
-                                  .pluck(:keyword)
         return MercariProduct.none if exclude_keywords.blank?
 
         exclude_keywords.map do |exclude_keyword|
           MercariProduct.where("name LIKE ?", "%#{exclude_keyword}%")
         end.reduce(&:or)
+      end
+
+      def required_keywords
+        @required_keywords ||=
+          product.mercari_crawl_setting
+                 .mercari_crawl_setting_required_keywords
+                 .pluck(:keyword)
+                 .map { |keyword| normalize_keyword(keyword) }
+      end
+
+      def exclude_keywords
+        @exclude_keywords ||=
+          product.mercari_crawl_setting
+                 .mercari_crawl_setting_exclude_keywords
+                 .pluck(:keyword)
+                 .map { |keyword| normalize_keyword(keyword) }
+      end
+
+      def normalize_keyword(keyword)
+        keyword.tr("Ａ-Ｚ０-９", "A-Z0-9").downcase
       end
     end
   end
