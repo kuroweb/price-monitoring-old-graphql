cd68581ab5589e06ee209dad96d1245c133e6b85
kuroweb <kuromitsu0104@gmail.com>
2024-04-02 22:10:53 +0900

    feat: 関連モデル取得エンドポイントを追加

---
+++ volumes/backend/app/controllers/api/v1/products/related_products_controller.rb
@@ -0,0 +1,22 @@
+module Api
+  module V1
+    module Products
+      class RelatedProductsController < ApplicationController
+        def index
+          render json: { related_products: Search::RetrieveRelatedProduct.call(params: search_params) },
+                 status: 200
+        end
+
+        private
+
+        def product
+          @product ||= Product.find(params[:product_id])
+        end
+
+        def search_params
+          params.permit(:product_id, :published, :page, :per)
+        end
+      end
+    end
+  end
+end
--- volumes/backend/config/routes.rb
+++ volumes/backend/config/routes.rb
@@ -1,7 +1,7 @@
 require "sidekiq/web"
 require "sidekiq/cron/web"
 
-Rails.application.routes.draw do
+Rails.application.routes.draw do # rubocop:disable Metrics/BlockLength
   namespace :api do
     namespace :v1 do
       resources :products
@@ -25,6 +25,9 @@ Rails.application.routes.draw do
             resources :mercari_crawl_setting_required_keywords, only: %i[index create update destroy]
           end
           resources :mercari_daily_purchase_summaries, only: [:index]
+
+          ## related_product ##
+          resources :related_products, only: %i[index]
         end
       end
     end
