4a8e3a3338da00087c280bb4f26d1007a19449ca
kuroweb <kuromitsu0104@gmail.com>
2024-04-06 16:37:53 +0900

    feat: label追加

--- volumes/frontend/features/products/components/SearchForm.tsx
+++ volumes/frontend/features/products/components/SearchForm.tsx
@@ -17,24 +17,34 @@ const SearchForm = () => {
 
   return (
     <>
-      <div className='space-x-4'>
-        <select
-          className='select select-bordered w-28'
-          onChange={handlePublishedChange}
-          value={published ? 'true' : 'false'}
-        >
-          <option value='true'>出品中</option>
-          <option value='false'>売り切れ</option>
-        </select>
-        <select
-          className='select select-bordered w-28'
-          onChange={handlePerChange}
-          value={per.toString()}
-        >
-          <option value='10'>10件</option>
-          <option value='50'>50件</option>
-          <option value='100'>100件</option>
-        </select>
+      <div className='flex flex-wrap space-x-4'>
+        <label className='form-control w-28'>
+          <div className='label'>
+            <span className='label-text'>ステータス</span>
+          </div>
+          <select
+            className='select select-bordered'
+            onChange={handlePublishedChange}
+            value={published ? 'true' : 'false'}
+          >
+            <option value='true'>出品中</option>
+            <option value='false'>売り切れ</option>
+          </select>
+        </label>
+        <label className='form-control w-28'>
+          <div className='label'>
+            <span className='label-text'>表示数</span>
+          </div>
+          <select
+            className='select select-bordered w-28'
+            onChange={handlePerChange}
+            value={per.toString()}
+          >
+            <option value='10'>10件</option>
+            <option value='50'>50件</option>
+            <option value='100'>100件</option>
+          </select>
+        </label>
       </div>
     </>
   )
