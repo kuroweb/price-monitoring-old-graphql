091ec2fffddcf7ef7d8d748120918a2ad0108da3
kuroweb <kuromitsu0104@gmail.com>
2024-05-02 17:00:55 +0900

    refactor: update

--- volumes/backend/app/services/crawl/client.rb
+++ volumes/backend/app/services/crawl/client.rb
@@ -1,51 +1,93 @@
 module Crawl
   class Client
-    COMMON_BLOCKED_LIST = [".css"].freeze
-    MERCARI_BLOCKED_LIST = [
-      "common.json", "mypage.json", "seo.json", "search.json", "validation.json", "sell.json", "brand.json",
-      "user.json", "www.google.com", "googletagmanager.com", "securepubads.g.doubleclick.net", "sentry.io"
-    ].freeze
-    YAHOO_AUCTION_BLOCKED_LIST = [
-      "yads.c.yimg.jp", "yads.yjtag.yahoo.co.jp", "www.googleadservices.com", "static.criteo.net", "clb.yahoo.co.jp",
-      "mhd.yahoo.co.jp", "s.yimg.jp", "yjtag.yahoo.co.jp", "s.yjtag.jp", "facebook.net"
-    ].freeze
-    JANPARA_BLOCKED_LIST = [
-      "https://www.janpara.co.jp/javascripts", "https://www.janpara.co.jp/common", "https://www.janpara.co.jp/poplink",
-      "apis.google.com", "m.geniee-search.net", "pagead2.googlesyndication.com", "flux-cdn.com"
+    COMMON_BLOCKED_LIST = %w[.css].freeze
+
+    MERCARI_BLOCKED_LIST = %w[
+      common.json
+      mypage.json
+      seo.json
+      search.json
+      validation.json
+      sell.json
+      brand.json
+      user.json
+      www.google.com
+      googletagmanager.com
+      securepubads.g.doubleclick.net
+      sentry.io
     ].freeze
 
-    UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) " \
-         "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36".freeze
+    YAHOO_AUCTION_BLOCKED_LIST = %w[
+      yads.c.yimg.jp
+      yads.yjtag.yahoo.co.jp
+      www.googleadservices.com
+      static.criteo.net
+      clb.yahoo.co.jp
+      mhd.yahoo.co.jp
+      s.yimg.jp
+      yjtag.yahoo.co.jp
+      s.yjtag.jp
+      facebook.net
+    ].freeze
 
-    def self.execute(&block) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
-      server, username, password = Proxy.get
+    JANPARA_BLOCKED_LIST = %w[
+      www.janpara.co.jp/javascripts
+      www.janpara.co.jp/common
+      www.janpara.co.jp/poplink
+      apis.google.com
+      m.geniee-search.net
+      pagead2.googlesyndication.com
+      flux-cdn.com
+    ].freeze
 
-      Playwright.connect_to_playwright_server(ENV.fetch("PLAYWRIGHT_URL")) do |playwright|
-        playwright.chromium.launch(
+    USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1) " \
+                 "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36".freeze
+
+    class << self
+      def execute(&block)
+        Playwright.connect_to_playwright_server(ENV.fetch("PLAYWRIGHT_URL")) do |playwright|
+          playwright.chromium.launch(**launch_options) do |browser|
+            browser.new_context(**context_options) do |context|
+              page = context.new_page
+
+              page.route(
+                "**/*",
+                lambda do |route, request|
+                  return route.abort if blocked_request?(request.url)
+
+                  route.fallback
+                end
+              )
+
+              block.call(page)
+            end
+          end
+        end
+      end
+
+      def launch_options
+        server, username, password = Proxy.get
+
+        {
           headless: true,
           proxy: { server:, username:, password: },
           args: ["--blink-settings=imagesEnabled=false", "--disable-remote-fonts"]
-        ) do |browser|
-          browser.new_context(
-            userAgent: UA
-          ) do |context|
-            page = context.new_page
+        }
+      end
 
-            page.route(
-              "**/*",
-              lambda { |route, request|
-                return route.abort if COMMON_BLOCKED_LIST.any? { |url| request.url.include?(url) }
-                return route.abort if MERCARI_BLOCKED_LIST.any? { |url| request.url.include?(url) }
-                return route.abort if YAHOO_AUCTION_BLOCKED_LIST.any? { |url| request.url.include?(url) }
-                return route.abort if JANPARA_BLOCKED_LIST.any? { |url| request.url.include?(url) }
+      def context_options
+        { userAgent: USER_AGENT }
+      end
 
-                route.fallback
-              }
-            )
+      def blocked_request?(url)
+        blocked_url?(COMMON_BLOCKED_LIST, url) \
+          || blocked_url?(MERCARI_BLOCKED_LIST, url) \
+          || blocked_url?(YAHOO_AUCTION_BLOCKED_LIST, url) \
+          || blocked_url?(JANPARA_BLOCKED_LIST, url)
+      end
 
-            block.call(page)
-          end
-        end
+      def blocked_url?(blocked_list, url)
+        blocked_list.any? { |blocked_url| url.include?(blocked_url) }
       end
     end
   end
