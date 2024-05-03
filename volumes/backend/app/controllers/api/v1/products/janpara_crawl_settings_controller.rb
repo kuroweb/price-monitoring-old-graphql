0f61044ee3afb2b4cd1c1cb030eb87740295d15f
kuroweb <kuromitsu0104@gmail.com>
2024-05-03 17:55:35 +0900

    refactor: update

--- volumes/backend/app/controllers/api/v1/products/janpara_crawl_settings_controller.rb
+++ volumes/backend/app/controllers/api/v1/products/janpara_crawl_settings_controller.rb
@@ -5,11 +5,7 @@ module Api
         def index
           janpara_crawl_setting = JanparaCrawlSetting.find_by(product_id: params[:product_id])
 
-          if janpara_crawl_setting
-            render json: janpara_crawl_setting, status: 200
-          else
-            render json: { message: "Requested resource was not found." }, status: 404
-          end
+          render json: janpara_crawl_setting, status: 200
         end
       end
     end
