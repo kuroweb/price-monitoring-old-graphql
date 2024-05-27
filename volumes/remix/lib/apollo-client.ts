fe3b35d67129ba19761f4da26610beb569181a7d
kuroweb <kuromitsu0104@gmail.com>
2024-05-28 01:09:50 +0900

    feat: apollo-client追加

--- volumes/remix/lib/apollo-client.ts
+++ volumes/remix/lib/apollo-client.ts
@@ -3,6 +3,12 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 const client = new ApolloClient({
   uri: process.env.BFF_URL,
   cache: new InMemoryCache(),
+  defaultOptions: {
+    watchQuery: {
+      // 暫定対応でキャッシュ無効化
+      fetchPolicy: 'no-cache'
+    }
+  }
 })
 
 export default client
