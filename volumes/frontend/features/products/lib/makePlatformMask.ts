cf0202848b01dd584b6c1aea8949bcd2b3b078be
kuroweb <kuromitsu0104@gmail.com>
2024-06-04 06:50:13 +0900

    refactor: update

--- volumes/frontend/features/products/lib/makePlatformMask.ts
+++ volumes/frontend/features/products/lib/makePlatformMask.ts
@@ -4,8 +4,7 @@ export const makePlatformMask = (platform: string, status: string) => {
 
   const platformMaskMap: { [key: string]: { [key: string]: string } } = {
     all: {
-      published:
-        'yahoo_auction.buyable,yahoo_fleamarket.published,mercari.published,janpara.all,iosys.all,pc_koubou.all,used_sofmap.all',
+      published: defaultMask,
       unpublished: 'yahoo_auction.unpublished,yahoo_fleamarket.unpublished,mercari.unpublished',
     },
     yahoo_auction: { published: 'yahoo_auction.buyable', unpublished: 'yahoo_auction.unpublished' },
@@ -17,10 +16,7 @@ export const makePlatformMask = (platform: string, status: string) => {
     janpara: { published: 'janpara.all', unpublished: 'janpara.all' },
     iosys: { published: 'iosys.all', unpublished: 'iosys.all' },
     pc_koubou: { published: 'pc_koubou.all', unpublished: 'pc_koubou.all' },
-    used_sofmap: { published: 'used_sofmap.all', unpublished: 'used_sofmap.all' },
   }
 
-  const platformMask = platformMaskMap[platform]?.[String(status)]
-
-  return platformMask || defaultMask
+  return platformMaskMap[platform]?.[status] || defaultMask
 }
