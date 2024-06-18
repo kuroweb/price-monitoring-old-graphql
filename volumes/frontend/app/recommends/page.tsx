3b4fd4f682bfe5b647f2bad325a6300994842d1e
kuroweb <kuromitsu0104@gmail.com>
2024-06-18 23:10:09 +0900

    feat: レイアウト調整

--- volumes/frontend/app/recommends/page.tsx
+++ volumes/frontend/app/recommends/page.tsx
@@ -15,22 +15,26 @@ const Page = async () => {
   return (
     <Layout>
       <div className='grid grid-cols-1'>
-        {data.products.map((product) => (
-          <>
-            <Link className='card-title' href={`/products/${product.id}`}>
-              {product.name}
-            </Link>
-            <div className='grid auto-cols-[10rem] grid-flow-col gap-2 overflow-x-auto pt-4'>
-              {product.relatedProducts.map((relatedProduct) => (
-                <RelatedProductCard
-                  key={relatedProduct.externalId}
-                  relatedProduct={relatedProduct}
-                />
-              ))}
-            </div>
-            <div className="divider" />
-          </>
-        ))}
+        <div className='card bg-neutral'>
+          <div className='card-body'>
+            {data.products.map((product) => (
+              <>
+                <Link className='card-title' href={`/products/${product.id}`}>
+                  {product.name}
+                </Link>
+                <div className='grid auto-cols-[10rem] grid-flow-col gap-2 overflow-x-auto pt-4'>
+                  {product.relatedProducts.map((relatedProduct) => (
+                    <RelatedProductCard
+                      key={relatedProduct.externalId}
+                      relatedProduct={relatedProduct}
+                    />
+                  ))}
+                </div>
+                <div className='divider my-1' />
+              </>
+            ))}
+          </div>
+        </div>
       </div>
     </Layout>
   )
