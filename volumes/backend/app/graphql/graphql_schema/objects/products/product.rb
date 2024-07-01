bf62b048762a682c6579d1907938159000d1764e
kuroweb <kuromitsu0104@gmail.com>
2024-07-01 09:14:04 +0900

    feat: 親カテゴリをセレクタで指定して登録できるようにする

--- volumes/backend/app/graphql/graphql_schema/objects/products/product.rb
+++ volumes/backend/app/graphql/graphql_schema/objects/products/product.rb
@@ -18,6 +18,7 @@ module GraphqlSchema
         field :yahoo_fleamarket_daily_purchase_summaries, [YahooFleamarketDailyPurchaseSummary], null: false
         field :mercari_daily_purchase_summaries, [MercariDailyPurchaseSummary], null: false
         field :related_products, resolver: Resolvers::Products::RelatedProducts
+        field :category, Categories::Category, null: false
 
         def yahoo_auction_crawl_setting
           object.yahoo_auction_crawl_setting
@@ -53,6 +54,10 @@ module GraphqlSchema
 
         def mercari_daily_purchase_summaries
           object.mercari_daily_purchase_summaries
+        end
+
+        def category
+          object.category
         end
       end
     end
