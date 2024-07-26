ef8348afc02b003050bc7024fc5569c8cea71918
kuroweb <kuromitsu0104@gmail.com>
2024-07-27 00:32:29 +0900

    feat: add validations

--- volumes/backend/spec/models/product_spec.rb
+++ volumes/backend/spec/models/product_spec.rb
@@ -1,11 +1,29 @@
 require "rails_helper"
 
 RSpec.describe Product, type: :model do
-  it "validates presence of name" do
-    product = Product.new
-    product.valid?
-    expect(product.errors[:name]).to include("can't be blank")
+  describe "associations" do
+    it { is_expected.to have_one(:yahoo_auction_crawl_setting).dependent(:destroy) }
+    it { is_expected.to have_one(:mercari_crawl_setting).dependent(:destroy) }
+    it { is_expected.to have_one(:janpara_crawl_setting).dependent(:destroy) }
+    it { is_expected.to have_one(:iosys_crawl_setting).dependent(:destroy) }
+    it { is_expected.to have_one(:pc_koubou_crawl_setting).dependent(:destroy) }
+    it { is_expected.to have_one(:used_sofmap_crawl_setting).dependent(:destroy) }
+    it { is_expected.to have_one(:product_category_map).dependent(:destroy) }
+    it { is_expected.to have_one(:category).through(:product_category_map) }
+
+    it { is_expected.to have_many(:yahoo_auction_products).dependent(:destroy) }
+    it { is_expected.to have_many(:yahoo_auction_daily_purchase_summaries).dependent(:destroy) }
+    it { is_expected.to have_many(:yahoo_fleamarket_products).dependent(:destroy) }
+    it { is_expected.to have_many(:yahoo_fleamarket_daily_purchase_summaries).dependent(:destroy) }
+    it { is_expected.to have_many(:mercari_products).dependent(:destroy) }
+    it { is_expected.to have_many(:mercari_daily_purchase_summaries).dependent(:destroy) }
+    it { is_expected.to have_many(:janpara_products).dependent(:destroy) }
+    it { is_expected.to have_many(:iosys_products).dependent(:destroy) }
+    it { is_expected.to have_many(:pc_koubou_products).dependent(:destroy) }
+    it { is_expected.to have_many(:used_sofmap_products).dependent(:destroy) }
   end
 
-  # Add more test cases as needed
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+  end
 end
