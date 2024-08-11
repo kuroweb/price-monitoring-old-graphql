33b41d1aef505d4216e9443eff772427b3c2d78d
kuroweb <kuromitsu0104@gmail.com>
2024-08-11 18:59:48 +0900

    debug

--- volumes/backend/app/models/category.rb
+++ volumes/backend/app/models/category.rb
@@ -19,6 +19,8 @@ class Category < ApplicationRecord
     return if root?
     return if parent.depth <= DEPTH_LIMIT
 
+    # debug
+
     errors.add(:base, "Depth limit exceeded. Maximum depth is #{DEPTH_LIMIT}.")
   end
 end
