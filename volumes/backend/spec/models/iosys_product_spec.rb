20b0f071d6e51c522521f6164b792e6e1317ab58
kuroweb <kuromitsu0104@gmail.com>
2024-08-12 23:24:10 +0900

    feat: add rspec

---
+++ volumes/backend/spec/models/iosys_product_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe IosysProduct, type: :model do
+  describe "associations" do
+    it { is_expected.to belong_to(:product) }
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:external_id) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:thumbnail_url) }
+  end
+end
---
+++ volumes/backend/spec/models/yahoo_auction_product_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe YahooAuctionProduct, type: :model do
+  describe "associations" do
+    it { is_expected.to belong_to(:product) }
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:external_id) }
+    it { is_expected.to validate_presence_of(:seller_id) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:thumbnail_url) }
+  end
+end
