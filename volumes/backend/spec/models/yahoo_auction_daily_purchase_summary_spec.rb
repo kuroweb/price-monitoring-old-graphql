a47ded1b5ac32108daf23d7d1c517642c7575a3d
kuroweb <kuromitsu0104@gmail.com>
2024-08-12 23:43:11 +0900

    feat: add rspec

---
+++ volumes/backend/spec/models/yahoo_auction_daily_purchase_summary_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+
+RSpec.describe YahooAuctionDailyPurchaseSummary, type: :model do
+  describe "associations" do
+    it { is_expected.to belong_to(:product) }
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:purchase_count) }
+    it { is_expected.to validate_presence_of(:date) }
+  end
+end
