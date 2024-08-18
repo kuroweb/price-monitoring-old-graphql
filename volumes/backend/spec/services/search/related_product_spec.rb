241da14b2f540be10f3f9587ef08a0c51a80b029
kuroweb <kuromitsu0104@gmail.com>
2024-08-18 22:28:15 +0900

    feat: add spec

--- volumes/backend/spec/services/search/related_product_spec.rb
+++ volumes/backend/spec/services/search/related_product_spec.rb
@@ -204,6 +204,31 @@ RSpec.describe Search::RelatedProduct, type: :service do
           end
         end
       end
+
+      context "iosysを指定した場合" do
+        let!(:iosys_product_a) { create(:iosys_product, product:, name: "IosysProduct A") }
+        let!(:iosys_product_b) { create(:iosys_product, product:, name: "IosysProduct B") }
+
+        context "iosys.allの場合" do
+          let!(:platform_mask) { "iosys.all" }
+          let!(:expected_products) { [iosys_product_a, iosys_product_b] }
+
+          it "すべてのJanparaProductの値を返却すること" do
+            service = described_class.new(params: { product_id: product.id, platform_mask:,
+                                                    sort: "created_at", order: "asc" })
+            actual_array = service.call.products
+            expected_array = expected_products.map do |product|
+              product.attributes
+                     .slice(*RelatedProduct.attribute_names)
+                     .merge("platform" => "iosys")
+            end
+
+            expected_array.each_with_index do |expected, index|
+              expect(actual_array[index]).to have_attributes(expected)
+            end
+          end
+        end
+      end
     end
   end
 end
